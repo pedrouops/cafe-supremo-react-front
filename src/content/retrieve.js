dc991d00942b965a66d1596c540a77bd3e6da3af
Dolf Dijkstra <dolfdijkstra@users.noreply.github.com>
2018-03-06 12:26:59 +0100

    initial baby step for prefetch

--- src/content/retrieve.js
+++ src/content/retrieve.js
@@ -6,10 +6,19 @@ const accessToken = '11ad271cc1ae61bd03249332e8445c96'
 //      "search": "?field:type:equals=Ad&contentType=published&fields=ALL&orderBy=updateddate%3Adesc&limit=1"
 //
 
-search = {
-  'field:type:equals': 'Ad',
+
+
+
+const retrieve = ({contentType, orderBy='updateddate:desc', limit=3}) => {
+    const key = [contentType,orderBy,limit].join('|')
+    
+ const search = {
+  'field:type:equals': contentType,
   contentType: 'published',
   fields: 'ALL',
-  orderBy: 'updateddate%3Adesc',
-  limit: 1
+  orderBy: orderBy,
+  limit: limit
+}
+    
+    
 }
