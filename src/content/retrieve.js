59464e26818120f4ddd4595d5ae7a03dc9f4e4e9
Dolf Dijkstra <dolfdijkstra@users.noreply.github.com>
2018-03-07 21:58:25 +0100

    ran formatter

--- src/content/retrieve.js
+++ src/content/retrieve.js
@@ -6,19 +6,14 @@ const accessToken = '11ad271cc1ae61bd03249332e8445c96'
 //      "search": "?field:type:equals=Ad&contentType=published&fields=ALL&orderBy=updateddate%3Adesc&limit=1"
 //
 
+const retrieve = ({ contentType, orderBy = 'updateddate:desc', limit = 3 }) => {
+  const key = [contentType, orderBy, limit].join('|')
 
-
-
-const retrieve = ({contentType, orderBy='updateddate:desc', limit=3}) => {
-    const key = [contentType,orderBy,limit].join('|')
-    
- const search = {
-  'field:type:equals': contentType,
-  contentType: 'published',
-  fields: 'ALL',
-  orderBy: orderBy,
-  limit: limit
-}
-    
-    
+  const search = {
+    'field:type:equals': contentType,
+    contentType: 'published',
+    fields: 'ALL',
+    orderBy: orderBy,
+    limit: limit
+  }
 }
