8488e19ccbc499f6e9101d1b35178a2babbf4c0f
Pedro Almeida <pedro.almeida@eproseed.com>
2018-03-16 08:17:50 +0000

    step 2 done

--- src/step2/Layout.jsx
+++ src/step2/Layout.jsx
@@ -10,7 +10,7 @@ export default class Layout extends Component {
     return (
       <div>
         <h1>Hello Content</h1>
-        {items.map((item, index) => <div key={index}>PUT NAME HERE</div>)}
+        {items.map((item, index) => <div key={index}>{item.name}</div>)}
       </div>
     )
   }
